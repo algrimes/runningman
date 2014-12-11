8b4428796c636edb261d27952a9029ecf01812a3
algrimes <agrimes@thoughtworks.com>
2014-12-11 16:02:49 +1100

    Al : Slim down options

--- grunt/tasks/connect.js
+++ grunt/tasks/connect.js
@@ -1,9 +1,7 @@
 module.exports = {
 	server: {
     	options: {
-        	hostname: '*',
 			keepalive: true,
-			port: 8000,
 			middleware: function (connect, options) {
 				return [connect.static(options.base[0])];
 			}
